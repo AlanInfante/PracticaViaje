202aea93ab52ba99c48a6ccde9ac7b902e22629e
alan <infantelobo@gmail.com>
2026-04-22 11:20:40 -0300

    prrimer Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*const contador = document.querySelector("#contar");
+let contadorClicks = 0;
+
+contador.addEventListener("click", () => {
+    contadorClicks++;
+    console.log(`Has hecho clic ${contadorClicks} veces`);
+    document.querySelector("#numero").textContent = contadorClicks;
+});*/
+const formulario = document.querySelector("#Viaje");
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const nombre = document.querySelector("#nombre").value;
+    const apellido = document.querySelector("#apellido").value;
+    const mensaje = `Hola ${nombre}, ${apellido} queres ir conmigo a BRASIL?`;
+    document.querySelector("#mensaje").textContent = mensaje;
+});
